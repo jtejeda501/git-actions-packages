2a81cee168a7177f56c888639cfccdc5947cd46f
julioTejeda-web <jtejeda@whipmedia.com>
2025-06-11 12:00:27 -0600

    remove all the unnecessary dependencies

--- index.js
+++
@@ -1,7 +0,0 @@
-import * as ss from 'simple-statistics';
-
-const data = [1, 2, 3, 4, 5, 6];
-
-console.log('Mean:', ss.mean(data));
-console.log('Median:', ss.median(data));
-console.log('Standard Deviation:', ss.standardDeviation(data));
